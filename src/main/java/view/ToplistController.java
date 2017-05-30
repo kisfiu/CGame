4d8450fed3a59bdb565e07957a4e3036b366cb58
kisfiu <kristofkovacs25@gmail.com>
2017-05-30 02:03:19 +0200

    toplist

--- src/main/java/view/ToplistController.java
+++ src/main/java/view/ToplistController.java
@@ -73,23 +73,9 @@ public class ToplistController
     @FXML private Label points3; @FXML private Label points8; 
     @FXML private Label points4; @FXML private Label points9; 
     @FXML private Label points5; @FXML private Label points10; 
-	
-//    GameController gamecontroller = loader.getController();
-    
-//        SetToplist.valami();
-
-    
-    public String asd;
     
     @FXML
     Button muta;
-//    @FXML
-//    private void mutasd () throws TransformerException, ParserConfigurationException, IOException, SAXException
-//    {
-//    	points1.setText("1");
-//    }
-//    
-//    
     @FXML
     public void mutasd() throws TransformerException, ParserConfigurationException, IOException, SAXException 
     {
@@ -97,15 +83,15 @@ public class ToplistController
 		domFactory.setIgnoringComments(true);  
 		DocumentBuilder builder = domFactory.newDocumentBuilder();   
 		Document doc = builder.parse(new File("src/main/java/model/merged.xml"));   
-	
 		NodeList nodes = doc.getElementsByTagName("player");  
 		
-		int[] array = new int[nodes.getLength()];
+		int[] arraypontok = new int[nodes.getLength()];
+		int[] arraypontokeredeti = new int[nodes.getLength()];
 		String[] arraynev = new String[nodes.getLength()];
-		String frommerged;
+		String[] arrayneveredeti = new String[nodes.getLength()];
+		String frommergedpont;
 		String frommergednev;
 		int Ifrommerged;
-		int osszehas;
 		int k = 0;
 		
 		
@@ -113,77 +99,77 @@ public class ToplistController
 		{	
 			org.w3c.dom.Node Nmerged = nodes.item(j);
 			Element Emerged = (Element) Nmerged;
-	        String nevmerged = Emerged.getAttribute("name");
+			String nevmerged = Emerged.getAttribute("name");
 			System.out.println(nevmerged);
 			
-			frommerged = Emerged.getElementsByTagName("points").item(0).getTextContent();
+			frommergedpont = Emerged.getElementsByTagName("points").item(0).getTextContent();
 			frommergednev = Emerged.getAttribute("name");
 			arraynev[k]= frommergednev;
+			arrayneveredeti[k]= frommergednev;
 
 			
-			Ifrommerged = Integer.parseInt(frommerged);
-			array[k]= Ifrommerged;
+			Ifrommerged = Integer.parseInt(frommergedpont);
+			arraypontok[k]= Ifrommerged;
+			arraypontokeredeti[k]= Ifrommerged;
 			k++;
 		}
-		int[] ezaregi = array;
-		Arrays.sort(array);		
-		System.out.println("\n_________\n" + Arrays.toString(ezaregi));
-
-		System.out.println(Arrays.toString(array));
-		
-		
-		// in reverse order
-//		System.out.println(array);
-		for (int i = array.length - 1; i >= 0; i--)
-		    System.out.print(array[i] + " ");
-		
-		
-		
-		for(int i = 0; i < array.length / 2; i++)
+		Arrays.sort(arraypontok);
+		System.out.println(Arrays.toString(arraypontok));
+		for(int i = 0; i < arraypontok.length / 2; i++)
 		{
-		    int temp = array[i];
-		    array[i] = array[array.length - i - 1];
-		    array[array.length - i - 1] = temp;
+		    int temp = arraypontok[i];
+		    arraypontok[i] = arraypontok[arraypontok.length - i - 1];
+		    arraypontok[arraypontok.length - i - 1] = temp;
 		}
-		System.out.println("\n\n" + array[0]);
-		System.out.println("\n_________\n" + Arrays.toString(arraynev));
-
-		System.out.println("\n_________\n" + Arrays.toString(ezaregi));
+		System.out.println(Arrays.toString(arraypontok));
+		System.out.println(Arrays.toString(arraypontokeredeti));
 		
-		String pont1 = Integer.toString(array[0]);
-		String pont2 = Integer.toString(array[1]);
-		String pont3 = Integer.toString(array[2]);
-		String pont4 = Integer.toString(array[3]);
-		String pont5 = Integer.toString(array[4]);
-		String pont6 = Integer.toString(array[5]);
-		String pont7 = Integer.toString(array[6]);
-		String pont8 = Integer.toString(array[7]);
-		String pont9 = Integer.toString(array[8]);
-		String pont10 = Integer.toString(array[9]);
+		String pont1 = Integer.toString(arraypontok[0]);	this.points1.setText(pont1);
+		String pont2 = Integer.toString(arraypontok[1]);	this.points2.setText(pont2);
+		String pont3 = Integer.toString(arraypontok[2]);	this.points3.setText(pont3);
+		String pont4 = Integer.toString(arraypontok[3]);	this.points4.setText(pont4);
+		String pont5 = Integer.toString(arraypontok[4]);	this.points5.setText(pont5);
+		String pont6 = Integer.toString(arraypontok[5]);	this.points6.setText(pont6);
+		String pont7 = Integer.toString(arraypontok[6]);	this.points7.setText(pont7);
+		String pont8 = Integer.toString(arraypontok[7]);	this.points8.setText(pont8);
+		String pont9 = Integer.toString(arraypontok[8]);	this.points9.setText(pont9);
+		String pont10 = Integer.toString(arraypontok[9]);	this.points10.setText(pont10);
 	
-		System.out.println(pont1);
+		
+		int l = 0;
+		for(int j=0; j<nodes.getLength(); j=j+1)
+		{	
+			org.w3c.dom.Node Nmerged = nodes.item(j);
+			Element Emerged = (Element) Nmerged;
 
-		this.points1.setText(pont1);
-		this.points2.setText(pont2);
-		this.points3.setText(pont3);
-		this.points4.setText(pont4);
-		this.points5.setText(pont5);
-		this.points6.setText(pont6);
-		this.points7.setText(pont7);
-		this.points8.setText(pont8);
-		this.points9.setText(pont9);
-		this.points10.setText(pont10);
+			String fFrommergedpont = Emerged.getElementsByTagName("points").item(0).getTextContent();
+			
+			for(int i=0; i<nodes.getLength(); i= i+1)
+			{
+				String nevmerged = Emerged.getAttribute("name");
+				
+				if(fFrommergedpont.equals(Integer.toString(arraypontok[i])))
+				{
+					arraynev[i]=nevmerged;
+					l++;
+				}
+
+			}
+			
+		}
 		
+		String nev1 = (arraynev[0]);	this.name1.setText(nev1);
+		String nev2 = (arraynev[1]);	this.name2.setText(nev2);
+		String nev3 = (arraynev[2]);	this.name3.setText(nev3);
+		String nev4 = (arraynev[3]);	this.name4.setText(nev4);
+		String nev5 = (arraynev[4]);	this.name5.setText(nev5);
+		String nev6 = (arraynev[5]);	this.name6.setText(nev6);
+		String nev7 = (arraynev[6]);	this.name7.setText(nev7);
+		String nev8 = (arraynev[7]);	this.name8.setText(nev8);
+		String nev9 = (arraynev[8]);	this.name9.setText(nev9);
+		String nev10 = (arraynev[9]);	this.name10.setText(nev10);
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println(Arrays.toString(arraynev));
 		
 		
 		System.out.println("\n\n_________________________toplistcreated_________________________");
@@ -191,4 +177,4 @@ public class ToplistController
     }	    
 	
     
- }
+}
